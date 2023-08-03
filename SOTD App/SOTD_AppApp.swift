3d3199d569a0946acf574b7f68d4b21b9c696272
Rashon <rashyslop@outlook.com>
2023-08-02 18:57:40 -0600

    Initial Commit

---
+++ SOTD App/SOTD_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOTD_AppApp.swift
+//  SOTD App
+//
+//  Created by Rashon Hyslop on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SOTD_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
